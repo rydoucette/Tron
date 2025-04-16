0cd9b820a593a73cac0f947f937194d1f8e6cdb4
rydoucette <ryanjldoucette2@gmail.com>
2025-04-16 17:30:13 -0300

    get rid of build directory

--- build/vendored/SDL_ttf/external/plutovg-build/CMakeFiles/plutovg.dir/source/plutovg-ft-math.c.o.d
+++
@@ -1,4 +0,0 @@
-vendored/SDL_ttf/external/plutovg-build/CMakeFiles/plutovg.dir/source/plutovg-ft-math.c.o: \
-  /Users/ryandoucette/Projects/Tron/vendored/SDL_ttf/external/plutovg/source/plutovg-ft-math.c \
-  /Users/ryandoucette/Projects/Tron/vendored/SDL_ttf/external/plutovg/source/plutovg-ft-math.h \
-  /Users/ryandoucette/Projects/Tron/vendored/SDL_ttf/external/plutovg/source/plutovg-ft-types.h
